592821e23c03a79d5df97046c5d83cabd89b4cb2
Harry Corradi <purestaticproductions@gmail.com>
2022-01-22 15:25:38 -0500

    fixed apostrophe issue in location. creating classes

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-# using tk starting the UI
+import events_classes
 
 import tkinter as tk
 
-# constants
+# CONSTANTS
+#
+# Tkinter
 DROPDOWN_PADDING = (15, 15)
 
-# window
-window = tk.Tk()
-window.title("Coaches Teams")
-window.config(padx=50, pady=50)
+events_data = "insert class"
 
 
-# this will be info from web scraping
-events = [1, 2, 3, 4, 5]
-teams = ["dynamic", "team 10", "team 20", "team 30", "team 40"]
 
-# Dropdowns
-# events *
-clicked_events = tk.StringVar()
-clicked_events.set("select events")
-drop_events = tk.OptionMenu(window, clicked_events, *events)
-drop_events.grid(row=0, column=0, pady=DROPDOWN_PADDING)
 
-# teams *
-clicked_teams = tk.StringVar()
-clicked_teams.set("select team")
-drop_teams = tk.OptionMenu(window, clicked_teams, *teams)
-drop_teams.grid(row=0, column=1, padx=DROPDOWN_PADDING)
 
-window.mainloop()
+
+# # TKINTER. most ikely will NOT be used. this is a good reference for operation
+# # window
+# window = tk.Tk()
+# window.title("Coaches Teams")
+# window.config(padx=50, pady=50)
+#
+#
+# # this will be info from web scraping
+# events = [1, 2, 3, 4, 5]
+# teams = ["dynamic", "team 10", "team 20", "team 30", "team 40"]
+#
+# # Dropdowns
+# # events *
+# clicked_events = tk.StringVar()
+# clicked_events.set("select events")
+# drop_events = tk.OptionMenu(window, clicked_events, *events)
+# drop_events.grid(row=0, column=0, pady=DROPDOWN_PADDING)
+#
+# # teams *
+# clicked_teams = tk.StringVar()
+# clicked_teams.set("select team")
+# drop_teams = tk.OptionMenu(window, clicked_teams, *teams)
+# drop_teams.grid(row=0, column=1, padx=DROPDOWN_PADDING)
+#
+# window.mainloop()
--- playground.py
+++ playground.py
@@ -16,48 +16,64 @@ import json
 DATE = "date"
 LOCATION = "local"
 URL = "url"
-
+"Player’s"
+"Player’'';,`~s"
 
 json_data = {"events": {
     "demo evnent":
-        {"date": "jan. 1, 2020", "location": "over the rainbow", "url": "/event/143"},
+        {"date": "jan. 1, 2020", "location": "over’s the rainbow  ******", "url": "/event/143"},
     "War Zone local":
         {"date": "may. 23, 2021", "location": "shack", "url": "/event/12"},
     "tourney":
-        {"date": "june. 3, 2023", "location": "utah", "url": "/event/105"},
+        {"date": "june. 3, 2023", "location": "utah's", "url": "/event/105"},
 }
              }
 
 
-json_data["events"]["new event"] = {DATE: "jan 1, 2011", LOCATION: "ohio", URL: "/event/123"}
+# json_data["events"]["new event"] = {DATE: "jan 1, 2011", LOCATION: "ohio", URL: "/event/123"}
 
 
 data_load = json.dumps(json_data, indent=4)
-# print(data_load)
+print(data_load)
+
+print()
+print("******")
+print()
+
+this = ["hello", "there", "this isn’t", "okay"]
+for x in this:
+    if "’" in x:
+        print("here")
+        y = x.replace("’", "fixed")
+        print(y)
+        continue
+    else:
+        pass
+    print(x)
 
 # with open(file="docs/tests/test_written_events.json", mode="w") as file:
 #     json.dump(json_data, file, indent=4)
 
-a = ["okay then /event/12/ here", "super/ev", "ghref= \"/event/234/\" tryinghere"]
+# a = ["okay then /event/12/ here", "super/ev", "ghref= \"/event/234/\" tryinghere"]
 
-this = []
-
-for x in a:
-    limbo = []
-    num = 0
-    if "/event/" in x:
-        print(x)
-        for z in x:
-            if z == "\"":
-                num += 1
-            if num == 2:
-                combined = "".join(limbo)
-                this.append(combined)
-                break
-            if num == 1 and z != "\"":
-                limbo.append(z)
-            else:
-                pass
-        # print(z)
-for h in this:
-    print(h)
+# this = []
+#
+# for x in a:
+#     limbo = []
+#     num = 0
+#     if "/event/" in x:
+#         print(x)
+#         for z in x:
+#             if z == "\"":
+#                 num += 1
+#             if num == 2:
+#                 combined = "".join(limbo)
+#                 this.append(combined)
+#                 break
+#             if num == 1 and z != "\"":
+#                 limbo.append(z)
+#             else:
+#                 pass
+#         # print(z)
+# for h in this:
+#     print(h)
